fcdc92a43c6d72347adab18fafd492489ac63e15
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-02-09 17:21:16 +0600

    add skeleton for the loading animation

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -12,24 +12,27 @@ export const FeedPosts = () => {
     return (
         <>
             <Container maxW={"container.sm"} py={10} px={2}>
-                {isLoading && [0, 1, 2, 3].map((_, idx) => {
+                {isLoading && [0, 1, 2, 3].map((_, idx) => (
                     <VStack key={idx} gap={4} alignItems={'flex-start'} mb={10}>
                         <Flex gap={2}>
                             <SkeletonCircle size={10} />
                             <VStack gap={2} alignItems={'flex-start'}>
                                 <Skeleton height={'10px'} w={"200px"}></Skeleton>
-                                <Skeleton height={'10px'} w={"200px"}></Skeleton>
+                                {/* <Skeleton height={'10px'} w={"200px"}></Skeleton> */}
                             </VStack>
                         </Flex>
                         <Skeleton w={'full'}>
-                            <Box h={'500px'}>contens wrapped</Box>
+                            <Box h={'500px'}>contents wrapped</Box>
                         </Skeleton>
                     </VStack>
-                })}
-                <FeedPost userName="burakormezz" img="/img1.png" avatar="/img1.png"></FeedPost>
-                <FeedPost userName="Jowh" img="/img2.png" avatar="/img2.png"></FeedPost>
-                <FeedPost userName="janedoe" img="/img3.png" avatar="/img3.png"></FeedPost>
-                <FeedPost userName="emily" img="/img4.png" avatar="/img4.png"></FeedPost>
+                ))}
+                {!isLoading && <>
+                    <FeedPost userName="burakormezz" img="/img1.png" avatar="/img1.png"></FeedPost>
+                    <FeedPost userName="Jowh" img="/img2.png" avatar="/img2.png"></FeedPost>
+                    <FeedPost userName="janedoe" img="/img3.png" avatar="/img3.png"></FeedPost>
+                    <FeedPost userName="emily" img="/img4.png" avatar="/img4.png"></FeedPost>
+                </>}
+
 
             </Container>
         </>
